4235d5d625c0c84757b2476a3a27408dca457b65
Beambear <liujp@bu.edu>
2023-07-01 18:28:35 -0400

    demo created

---
+++ demo/src/main/java/com/beambear/campusMarket/demo/CompusMarketApplication.java
@@ -0,0 +1,13 @@
+package com.beambear.campusMarket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompusMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompusMarketApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/beambear/campusMarket/demo/CompusMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beambear.campusMarket.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompusMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
